94b441c0527d6c079b3997f11b12f26d3b10c600
bakr <abab231196@gmail.com>
2025-03-04 21:09:18 +0200

    use .env

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -3,16 +3,16 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-config({
-  path: '.env.local',
-});
+config({ path: '.env' });
 
 const runMigrate = async () => {
-  if (!process.env.POSTGRES_URL) {
+  if (!process.env.DATABASE_URL) {
     throw new Error('POSTGRES_URL is not defined');
   }
 
-  const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
+  // const connection = postgres(process.env.POSTGRES_URL, { ssl: 'verify-full' });
+  const connection = postgres(process.env.DATABASE_URL, { ssl: 'verify-full' });
+
   const db = drizzle(connection);
 
   console.log('⏳ Running migrations...');
